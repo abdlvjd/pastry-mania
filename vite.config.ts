7393dfa84748339c037efbde6b2f9b42130d1388
vajid <abdulvajid987@gmail.com>
2025-10-22 07:30:56 +0530

    Update project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pastry-mania/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/pastry-mania/',
 })
